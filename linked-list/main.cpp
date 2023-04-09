4bece45447bed51980209d415617d58eb24eb2fd
Zakariae Lajoui <you@example.com>
2023-04-09 01:59:30 +0000

    split method

--- linked-list/main.cpp
+++ linked-list/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -10,71 +11,71 @@ using namespace std;
  *  - unshift: add to the beginning
  *  - shift: remove the first one
  *  - pop: remove the last one
-*/
+ */
 
-// Error Messages
+ // Error Messages
 string const EMPTY_ERR = "List is empty\n";
 string const NOT_EXISTED_ERR = "Node does not exist\n";
 
 class Node {
-    public:
-        float number;
-        Node *next;
-    
-        Node(float);
+public:
+    float number;
+    Node* next;
+
+    Node(float);
 };
 
-Node::Node(float number){
+Node::Node(float number) {
     this->number = number;
     this->next = NULL;
 }
 
 class LL {
-    private:
-        Node* head; // pointer to first element
-        int size;
+private:
+    Node* head; // pointer to first element
+    int size;
 
-    public:
-        LL(){ // Empty List
-            head = NULL;
-            size = 0;
-        }
+public:
+    LL() { // Empty List
+        head = NULL;
+        size = 0;
+    }
 
-        void add(float);
-        void unshift(float);
-        void remove(int);
-        void shift();
-        void pop();
-        void sort(string);
-        void displayAll();
+    // Data Manipulation
+    void add(float);
+    void unshift(float);
+    void remove(int);
+    void shift();
+    void pop();
+    void replace(float, float);
+    void replaceAll(float, float);
+    // Data Access
+    int get(float);
+    int getLast(float);
+    // 
+    float** split(int parts);
+    void sort(string);
+    void displayAll();
 };
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const* argv[]) {
     LL list;
-    
-    list.add(56);
-    list.add(10);
-    list.add(69);
 
-    list.unshift(7);
-    list.unshift(13);
+    for (int i = 1; i < 11; i++) list.add(rand());
 
     list.displayAll();
-
-    list.sort("ASC");
-
-    list.displayAll();
+    float** arr = list.split(2);
 }
 
-
-void LL::add(float number){
+void LL::add(float number) {
     Node* newItem = new Node(number);
-    
+
     if (head == NULL) { // first element
         head = newItem;
-    } else {
+    }
+    else {
         Node* current = head;
-        
+
         while (current->next != NULL) {
             current = current->next;
         }
@@ -85,12 +86,13 @@ void LL::add(float number){
     size++;
 }
 
-void LL::unshift(float number){
+void LL::unshift(float number) {
     Node* newItem = new Node(number);
-    
+
     if (head == NULL) { // first element
         head = newItem;
-    } else {
+    }
+    else {
         Node* tmp = head;
         head = newItem;
         newItem->next = tmp;
@@ -98,17 +100,19 @@ void LL::unshift(float number){
     size++;
 }
 
-void LL::remove(int pos){    
+void LL::remove(int pos) {
     if (pos >= size || pos < 0) { // Out of Scope
         cout << NOT_EXISTED_ERR;
-    } else if( pos == 0 ) { // First Element
+    }
+    else if (pos == 0) { // First Element
         this->pop();
-    } else {
+    }
+    else {
         Node* current = head;
 
-        for (int i = 0; i < pos - 1 ; i++)
+        for (int i = 0; i < pos - 1; i++)
             current = current->next;
-        
+
         Node* to_delete = current->next;
         current->next = to_delete->next;
         delete to_delete;
@@ -116,10 +120,11 @@ void LL::remove(int pos){
     }
 }
 
-void LL::shift(){
-    if (head == NULL){
+void LL::shift() {
+    if (head == NULL) {
         cout << NOT_EXISTED_ERR;
-    }else{
+    }
+    else {
         Node* to_delete = head;
         head = head->next;
         delete to_delete;
@@ -127,15 +132,16 @@ void LL::shift(){
     }
 }
 
-void LL::pop(){
-    if (head == NULL){
+void LL::pop() {
+    if (head == NULL) {
         cout << NOT_EXISTED_ERR;
-    }else{
+    }
+    else {
         Node* current = head;
 
         for (int i = 0; i < size - 2; i++)
             current = current->next;
-        
+
         Node* to_delete = current->next;
         current->next = NULL;
         delete to_delete;
@@ -143,8 +149,74 @@ void LL::pop(){
     }
 }
 
-void LL::sort(string order){
+int LL::get(float number) {
+    Node* current = head;
+    for (int i = 0; i < size; i++) {
+        if (current->number == number)
+            return i;
+        current = current->next;
+    }
+}
 
+int LL::getLast(float number) {
+    Node* current = head;
+    int found_index = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (current->number == number) found_index = i;
+
+        current = current->next;
+    }
+
+    return found_index;
+}
+
+void LL::replace(float number, float new_number) {
+    Node* current = head;
+
+    while (current != NULL) {
+        if (current->number == number) {
+            current->number = new_number;
+            return;
+        };
+
+        current = current->next;
+    }
+}
+
+void LL::replaceAll(float number, float new_number) {
+    Node* current = head;
+
+    while (current != NULL) {
+        if (current->number == number)
+            current->number = new_number;
+
+        current = current->next;
+    }
+}
+
+float** LL::split(int chunk_size) {
+    int number_of_chunks = ceil((float)size / chunk_size);
+
+    float** chunks = new float* [number_of_chunks];
+
+    Node* current = head;
+
+    for (int i = 0; i < number_of_chunks; i++) {
+        chunks[i] = new float[chunk_size];
+        for (int j = 0; j < chunk_size; j++) {
+            if (current != NULL) {
+                chunks[i][j] = current->number;
+                current = current->next;
+            }
+            else chunks[i][j] = 0;
+        }
+    }
+
+    return chunks;
+}
+
+void LL::sort(string order) {
     for (int i = 0; i < size - 1; i++) {
         Node* current = head;
         int changes = 0;
@@ -155,36 +227,36 @@ void LL::sort(string order){
 
             if (
                 (current_number > next_number and order == "ASC") or
-                (current_number < next_number and order == "DESC")
-                ) {
-                    // Swap
-                    current->number = next_number;
-                    current->next->number = current_number;
-                    changes++;
+                (current_number < next_number and order == "DESC")) {
+                // Swap
+                current->number = next_number;
+                current->next->number = current_number;
+                changes++;
             }
 
             current = current->next;
         }
 
-        if (changes == 0) break; // No changer = the list in the right order
-    }    
+        if (changes == 0)
+            break; // No changer = the list in the right order
+    }
 }
 
-void LL::displayAll(){
+void LL::displayAll() {
     Node* current = head;
     int count = 0;
-    cout << "-------------------" << endl;
+    cout << "*-------------------*" << endl;
     cout << "Size of List: " << size << endl;
-    cout << "-------------------" << endl;
+    cout << "*-------------------*" << endl;
 
     if (current == NULL)
         cout << EMPTY_ERR;
-    
+
     while (current != NULL) {
-        cout << count << ": " << current->number << endl ;
+        cout << count << ": " << current->number << endl;
         current = current->next;
         count++;
     }
 
-    cout << "-------------------" << endl;
+    cout << "*-------------------*" << endl;
 };
